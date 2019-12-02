4c201323a8a352e83f3f69d1524d863a548a01ef
zhanghe <15942892352@sina.cn>
2019-12-02 20:28:38 +0800

    feat:github第三方登录

--- routes/v1/githublogin/github.js
+++ routes/v1/githublogin/github.js
@@ -2,5 +2,5 @@ const router = require('koa-router')();
 const UserController = require('../../../controller/user');
 
 // github登录
-router.get('/oauth/redirect',UserController.oauth)
+router.get('/oauth/redirect', UserController.oauth);
 module.exports = router;
